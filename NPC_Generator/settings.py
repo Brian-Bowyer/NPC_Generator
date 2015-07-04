6d8afdedd0ce6699e543a2cf092e64706334bdf7
Black-Humor <bobsnake64@gmail.com>
2015-07-03 20:17:18 -0500

    Added basic HTML templates + CSS.

--- NPC_Generator/settings.py
+++ NPC_Generator/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'NPC_Gen',
 )
 
 MIDDLEWARE_CLASSES = (
