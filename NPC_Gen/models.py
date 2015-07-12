0ba9fe8481cc1b1d049eefd1e551fe749ad8ba84
Black-Humor <bobsnake64@gmail.com>
2015-07-12 14:15:36 -0500

    Added models.
    Also added TODOs, cleaned up template.

--- NPC_Gen/models.py
+++ NPC_Gen/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class NPC(models.Model):
+    name = models.CharField(max_length=255)
+    race = models.CharField(max_length=255)
+    char_class = models.CharField(max_length=255)
+    # TODO: Add alignment to NPC
+    # TODO: Add attributes to NPC
+    # TODO: Add skills to NPC
+    # TODO: Add HP and AC to NPC
--- NPC_Gen/views.py
+++ NPC_Gen/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 
+
 # Create your views here.
 def index(request):
     return render_to_response("NPC_Gen/index.html")
+
+#TODO: View that shows list of NPCs
